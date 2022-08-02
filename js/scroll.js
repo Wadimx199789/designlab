fe134804f60e411faa82f9c4f62c52e11aa897c7
Wadimx199789 <martinkewichwadim@gmail.com>
2022-08-02 12:21:24 +0300

    feat: update page

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,14 @@ const headerTopContainer = document.querySelector(".header__top-container");
 const labDesignBlock = document.querySelector(".lab");
 const footerMenuLinks = document.querySelectorAll(".footer__menu-link");
 
+const burger = document.querySelector(".header__burger");
+const closeMenuButton = document.querySelector(".header__nav-close");
+const headerNav = document.querySelector(".header__nav");
+const headerTop = document.querySelector(".header__top-container");
+const headerCloseBtn = document.querySelector(".header__nav-close");
+const headerLinks = document.querySelectorAll(".header__nav-link");
+const headerCall = document.querySelector(".header__call");
+
 
 function getCoords(block) {
   let box = block.getBoundingClientRect();
@@ -64,3 +72,31 @@ function scrollMenu(blockId) {
         })
     })
 }
+
+
+
+
+headerLinks.forEach((link)=>{
+    link.addEventListener("click",()=>{
+        burger.classList.remove("active");
+        headerNav.classList.remove("active");
+        headerTop.classList.remove("top");
+    })
+})
+burger.addEventListener("click",()=>{
+    console.log("click")
+    burger.classList.toggle("active");
+    if(burger.classList.contains("active")){
+        headerNav.classList.add("active");
+        headerTop.classList.add("top");
+    }
+    else{
+        headerNav.classList.remove("active"); 
+        headerTop.classList.remove("top");
+    }
+});
+headerCloseBtn.addEventListener("click",()=>{
+    burger.classList.remove("active");
+    headerNav.classList.remove("active");
+    headerTop.classList.remove("top");
+});
